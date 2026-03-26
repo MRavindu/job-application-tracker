a372d963aa7095362dcc03ae78efc2c6b385446c
MRavindu <ravindumedagama@gmail.com>
2026-03-26 23:50:59 +0530

    Add three images to the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,38 @@ export default function Home() {
           </div>
         </section>
 
+        {/* Hero Image Section */}
+        <section className="border-t bg-white py-16">
+          <div className="container mx-auto px-4">
+            <div className="mx-auto max-w-6xl">
+            {/* Tabs */}
+              <div className="flex gap-2 justify-center mb-8">
+                <Button>Organize Applications</Button>
+                <Button>Get Hired</Button>
+                <Button>Manage Boards</Button>
+              </div>
+              <div className="relative mx-auto max-w-5xl overflow-hidden rounded-lg border border-gray-200 shadow-xl">
+                <Image
+                  src="/hero-images/hero1.png"
+                  alt="Organize Applications"
+                  width={1200}
+                  height={800} />
+
+                  <Image
+                  src="/hero-images/hero2.png"
+                  alt="Manage All boards"
+                  width={1200}
+                  height={800} />
+
+                  <Image
+                  src="/hero-images/hero3.png"
+                  alt="Get Hired"
+                  width={1200}
+                  height={800} />
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
 
     </div>
